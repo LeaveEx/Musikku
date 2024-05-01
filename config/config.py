140d1491cbc8d5c51f52f1c91cae3baaf1250530
NOEL <80366713+jokokendi@users.noreply.github.com>
2024-05-01 18:43:15 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -42,6 +42,9 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 # A name for your Music bot.
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 
+# Join
+JOIN_DULU = getenv("JOIN_DULU")
+
 # Your User ID.
 OWNER_ID = list(
     map(int, getenv("OWNER_ID", "").split())
