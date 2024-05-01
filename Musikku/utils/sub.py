f20a06d907217e69372b1d474ed582ed882d8876
NOEL <80366713+jokokendi@users.noreply.github.com>
2024-05-01 18:38:30 +0700

    Update sub.py

--- Musikku/utils/sub.py
+++ Musikku/utils/sub.py
@@ -13,7 +13,7 @@ async def subcribe(client, message):
         await app.get_chat_member(config.JOIN_DULU, message.from_user.id)
     except UserNotParticipant:
         try:
-            link = await app.export_chat_invite_link(config.JOIN_DULU)
+            link = f"t.me/{config.JOIN_DULU}"
             await message.reply(
                 f"**Hay kak {message.from_user.mention}, Silahkan join dulu biar bisa pake bot ini kak**",
             reply_markup=InlineKeyboardMarkup(
