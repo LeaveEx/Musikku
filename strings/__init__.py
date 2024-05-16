7d511013701b6bb88d99767a03634c3948eff627
NOEL <80366713+jokokendi@users.noreply.github.com>
2024-05-17 00:26:09 +0700

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,19 +1,24 @@
 #
-# Copyright (C) 2021-2022 by kenkansaja@Github, < https://github.com/kenkansaja >.
+# Copyright (C) 2023-2024 by YukkiOwner@Github, < https://github.com/YukkiOwner >.
 #
-# This file is part of < https://github.com/kenkansaja/Musikku > project,
+# This file is part of < https://github.com/YukkiOwner/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/kenkansaja/Musikku/blob/master/LICENSE >
+# Please see < https://github.com/YukkiOwner/YukkiMusicBot/blob/master/LICENSE >
 #
 # All rights reserved.
+#
 
 import os
+import sys
 from typing import List
 
 import yaml
 
 languages = {}
 commands = {}
+
+
+languages_present = {}
 
 
 def get_command(value: str) -> List:
@@ -31,9 +36,29 @@ for filename in os.listdir(r"./strings"):
             open(r"./strings/" + filename, encoding="utf8")
         )
 
+
 for filename in os.listdir(r"./strings/langs/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
+        )
+        languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
+        if language_name == "en":
+            continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
+    try:
+        languages_present[language_name] = languages[language_name][
+            "name"
+        ]
+    except:
+        print(
+            "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
+        )
+        sys.exit(
